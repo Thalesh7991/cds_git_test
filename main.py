c4f589934af5c63fabcb1c4e122d073346ce3705
Thales Henrique <thalesh1997@gmail.com>
2023-01-05 14:09:02 -0300

    função de print criada

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 def gether_data():
     n1 = int(input("Primeiro Valor:"))
     n2 = int(input("Segundo Valor:"))
+
+    return n1,n2
+
+def print_message(n1,n2):
+    print(n1+n2)
+
+    return None
+
     
 
 def main():
     n1, n2 = gether_data()
-    print(n1**n2)
+    print_message(n1,n2)
     return None
 
 if __name__ == "__main__":
